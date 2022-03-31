02d42985dc52aa1b48fe0747604303efc456ccd8
Lars Henrik Ericson <glarrrrgh@gmail.com>
2022-03-31 15:58:33 +0200

    first working commit

--- windiskinfo.py
+++ windiskinfo.py
@@ -9,21 +9,12 @@ class Disk(object):
         """
 
     def __init__(self, physical_disk: wmi._wmi_object) -> None:
-        try:
-            self.size = int(physical_disk.Size)
-        except ValueError:
-            self.size = -1
-        try:
-            self.path = physical_disk.DeviceID
-            self.disk_number = int(self.path.split('PHYSICALDRIVE')[-1])
-        except IndexError:
-            self.path = ""
-            self.disk_number = -1
-        except ValueError:
-            self.path = ""
-            self.disk_number = -1
+        self._disk = physical_disk
+        self._set_size()
+        self._set_path()
         self.media_type = physical_disk.MediaType
         self.caption = physical_disk.Caption
+        self._set_partitions(physical_disk)
 
 
     def get_path(self) -> str:
@@ -52,9 +43,33 @@ class Disk(object):
         """Get model name and maybe manufacturer"""
         return self.caption
 
+    def _set_size(self) -> None:
+        """private: extract size from a wmi object"""
+        try:
+            self.size = int(self._disk.Size)
+        except ValueError:
+            self.size = -1
+
+    def _set_path(self) -> None:
+        """private: extract the disk path from a wmi object"""
+        try:
+            self.path = self._disk.DeviceID
+            self.disk_number = int(self.path.split('PHYSICALDRIVE')[-1])
+        except IndexError:
+            self.path = ""
+            self.disk_number = -1
+        except ValueError:
+            self.path = ""
+            self.disk_number = -1
+
     def __str__(self) -> str:
         """Overloading the string method"""
-        return ", ".join((self.get_caption(), self.get_path(), self.get_type(), self.get_size()))
+        disk = ", ".join((self.get_caption(), self.get_path(), self.get_type(), self.get_size()))
+        partitions = ["\n".join(["    " + line for line in str(partition).split("\n")]) for partition in self.partitions]
+        try:   
+            return "\n".join( (disk, *partitions ) )
+        except IndexError:
+            return disk
 
     def _set_partitions(self, physical_disk: wmi._wmi_object) -> None:
         """Private method that parses the partitons"""
@@ -70,26 +85,85 @@ class Partition(object):
     """
 
     def __init__(self, partition) -> None:
-        self._set_labels(partition)
-        self.size = partition.Size
-        
+        self._partition = partition
+        self._set_volumes()
+        self._set_size()
+        self._set_partition_table_type()
+        self._set_descriptor()
 
     def get_size(self) -> int:
         """Get the partition size in bytes."""
-        pass
+        return self.size
+
+    def get_descriptor(self) -> str:
+        """Get a descriptor of the partition"""
+        return self.descriptor
 
     def get_partiton_table_type(self) -> str:
         """get the partition table type"""
+        return self.partition_table_type
 
-    def get_filesystem_type(self):
-        """Get partition type."""
-        pass
+    def _set_descriptor(self) -> None:
+        """private: extract partition descriptor from a wmi object"""
+        self.descriptor = self._partition.Name
 
-    def _set_labels(self, partition: wmi._wmi_object) -> None:
-        """Private method that parse the logical disks"""
-        self.labels = [ each_label.Name for each_label in partition.associators('Win32_LogicalDiskToPartition')]
-        
+    def _set_volumes(self) -> None:
+        """Private: parse the logical disks"""
+        self.volumes = [ Volume(volume) for volume in self._partition.associators('Win32_LogicalDiskToPartition')]
 
+    def _set_size(self) -> None:
+        """private: extract size from a wmi object"""
+        try:
+            self.size = int(self._partition.Size)
+        except ValueError:
+            self.size = 0
+
+    def _set_partition_table_type(self) -> None:
+        """private: extract partition table type from a wmi object"""
+        self.partition_table_type = self._partition.Type
+
+    def __str__(self) -> str:
+        """overloading the __str__ method"""
+        partition = ", ".join((self.get_descriptor(), self.get_partiton_table_type(), str(self.get_size())))
+        volumes = []
+        volumes = ["\n".join(["    " + line for line in str(volume).split("\n")]) for volume in self.volumes]
+        try:
+            return "\n".join((partition, *volumes))
+        except IndexError:
+            return partition
+
+
+class Volume(object):
+    def __init__(self, volume) -> None:
+        self._volume = volume
+        self._set_name()
+        self._set_filesystem()
+
+    def get_file_system(self) -> str:
+        """Get filesystem type"""
+        return self.file_system
+
+    def get_name(self) -> str:
+        """Get the volume path letter."""
+        return self.name
+
+    def _set_filesystem(self) -> None:
+        """Private: Extracts the filesystem type from a wmi object"""
+        if isinstance(self._volume.FileSystem, str):
+            self.file_system = self._volume.FileSystem
+        else:
+            self.file_system = ""
+
+    def _set_name(self) -> None:
+        """Private: Extracts the volume name from a wmi object"""
+        if isinstance(self._volume.Name, str):
+            self.name = self._volume.Name
+        else:
+            self.name = ""
+    
+    def __str__(self) -> str:
+        """overloading the __str__ method"""
+        return ", ".join((self.get_name(), self.get_file_system()))
 
 
 def get_disks(computer: str = "", username: str = "", password: str = "") -> list:
@@ -107,9 +181,7 @@ def get_disks(computer: str = "", username: str = "", password: str = "") -> lis
         return None
     except wmi.x_wmi_authentication as err:
         return None
-    disks = []
-    for physical_disk in wmi_cursor.Win32_DiskDrive():
-        disks.append(Disk(physical_disk))
+    disks = [Disk(physical_disk) for physical_disk in wmi_cursor.Win32_DiskDrive()]
     return disks
 
 
