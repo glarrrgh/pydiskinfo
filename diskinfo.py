2d784a8f41d06b3042e8bc2987c51cb60a03404a
Lars Henrik Ericson <glarrrrgh@gmail.com>
2022-04-05 15:10:24 +0200

    Full revamp again. Added human readable values

--- diskinfo.py
+++ diskinfo.py
@@ -34,12 +34,76 @@ TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
+from logging.config import valid_ident
 import sys
 
 if sys.platform == 'win32':
     import wmi
 
-MEDIA_TYPES = []
+UNITS = { '': 1,
+          'K': 1_000, 
+          'M': 1_000_000, 
+          'G': 1_000_000_000, 
+          'T': 1_000_000_000_000, 
+          'P': 1_000_000_000_000_000,
+          'B': 1,
+          'KB': 1_000, 
+          'MB': 1_000_000, 
+          'GB': 1_000_000_000, 
+          'TB': 1_000_000_000_000, 
+          'PB': 1_000_000_000_000_000,
+          'KiB': 1_024, 
+          'MiB': pow(1_024, 2), 
+          'GiB': pow(1_024, 3), 
+          'TiB': pow(1_024, 4), 
+          'PiB': pow(1_024, 5)
+        }
+
+def human_readable_units(value: int, unit: str = 'auto', type: str = 'B') -> str:
+    """Converts an int to a string with a unit. 
+    
+       If unit is omitted, the function will choose a fitting unit.
+       If unit is omitted, and type is omitted, it will choose something like 
+       KB. type can be 
+            'B': decimal bytes (KB for instance)
+            'M': no bytes (K for instance)
+            'I': binary bytes (KiB for instance)
+       If unit is specified, type will be ignored.
+    """
+    if unit == 'auto':
+        if type == 'M':
+            for each_unit in ('P', 'T', 'G', 'M', 'K', ''):
+                return_value = value/UNITS[each_unit]
+                if return_value > 1:
+                    return_unit = each_unit
+                    break
+        elif type == 'I':
+            for each_unit in ('PiB', 'TiB', 'GiB', 'MiB', 'KiB', 'B'):
+                return_value = value/UNITS[each_unit]
+                if return_value > 1:
+                    return_unit = each_unit
+                    break
+        else:
+            for each_unit in ('PB', 'TB', 'GB', 'MB', 'KB', 'B'):
+                return_value = value/UNITS[each_unit]
+                if return_value > 1:
+                    return_unit = each_unit
+                    break        
+    else:
+        return_unit = unit
+        try:
+            return_value = value/UNITS[unit]
+        except KeyError:
+            raise ReadableUnitError
+    return "{:.3f}{}".format(return_value, return_unit)
+
+
+class ReadableUnitError(Exception):
+    """Exception raised if unit specified is not in dict UNITS"""
+    def __init__(self, message, status):
+        super().__init__(message, status)
+        self.message = "unit specified not found in UNITS"
+        self.status = status
 
 
 class DiskInfoParseError(Exception):
@@ -76,27 +140,42 @@ class System:
 
     def __init__(self, name: str = "System") -> None:
         self._name = name
-        self._drives = []
-        self._partitions = []
-        self._volumes = []
+        self._type = "generic"
+        self._disks = []
         self._parse_system()
+
+    def get_name(self) -> str:
+        """Return system name, as specified on instanciation"""
+        return self._name
+
+    def get_type(self) -> str:
+        """Return system/OS type. This value depends on the subclass used."""
+        return self._type
 
     def _parse_system(self) -> None:
         """To be overloaded by subclasses"""
         pass
 
     def __str__(self) -> str:
-        return repr(self._drives)
+        system = ", ".join(("System name: {}".format(self.get_name()), "System type/OS: {}".format(self.get_type())))
+        disks = ["\n".join(["  {}".format(line) for line in str(disk).split("\n")]) for disk in self._disks]
+        return "\n".join((system, *disks))
+
 
 class LinuxSystem(System):
     def _parse_system(self) -> None:
         pass
 
+
 class WindowsSystem(System):
     """This is an inherited version of the System class.
     
     This class will take care of the special cases when the module is runnning 
-    on windows."""  
+    on windows."""
+
+    def __init__(self, name: str = "Windows System") -> None:
+        super().__init__(name)
+        self._type = "Microsoft Windows"  
 
     def _parse_system(self) -> None:
         """Parse the system"""
@@ -106,18 +185,17 @@ class WindowsSystem(System):
             raise DiskInfoParseError from err
         except wmi.x_wmi_authentication as err:
             raise DiskInfoParseError from err
-        self._drives = [WindowsPhysicalDrive(drive) for drive in cursor.Win32_DiskDrive()]
-        self._volumes = [WindowsVolume(volume) for volume in cursor.Win32_DiskDrive())]
-        self._partitions = [WindowsPartition(partition) for partition in cursor.Win32_DiskPartition()]
+        self._disks = [WindowsPhysicalDisk(disk, self) for disk in cursor.Win32_DiskDrive()]
+        
 
-
-class PhysicalDrive:
+class PhysicalDisk:
     """Contains information about physical drives."""
 
-    def __init__(self) -> None:
+    def __init__(self, system: System) -> None:
+        self._system = system
         self._partitions = []
         self._size = 0
-        self._drive_number = 0
+        self._drive_number = -1
         self._path = ""
         self._media_type = ""
         self._serial_number = ""
@@ -127,85 +205,171 @@ class PhysicalDrive:
         self._cylinders = 0
         self._bytes_per_sector = 0
         self._firmware = ""
-        self._disk_number = -1
         self._interface_type = ""
         self._media_loaded = False
         self._status = ""
+
+    def get_system(self) -> System:
+        """Get the system that this disk is connected to."""
+        return self._system
     
-    def get_path(self) -> str:
-        """Get a path usable for raw access."""
-        return self.path
+    def get_partitions(self) -> list:
+        """Get a copy of the list of Partition objects connected to this disk."""
+        return self._partitions[:]
 
     def get_size(self) -> int:
         """Get the disk size in bytes."""
-        return str(self.size)
+        return self._size
 
+    def get_path(self) -> str:
+        """Get a path usable for raw access."""
+        return self._path
+
+    def get_media_type(self) -> str:
+        """Get the media type of this disk."""
+        return self._media_type
+
+    def get_serial_number(self) -> str:
+        """Get disk serial number."""
+        return self._serial_number
+
+    def get_model(self) -> str:
+        """Get disk model. This often includes hints of the manufacturer."""
+        return self._model
+
+    def get_sectors(self) -> int:
+        """Get the number of sectors on disk."""
+        return self._sectors
+
+    def get_heads(self) -> int:
+        """Get the number of heads on disk."""
+        return self._heads
+
+    def get_cylinders(self) -> int:
+        """Get the number of cylinders on disk."""
+        return self._cylinders
+
+    def get_bytes_per_sector(self) -> int:
+        """Get bytes per sector. This often translates to blocksize."""
+        return self._bytes_per_sector
+
+    def get_firmware_version(self) -> str:
+        """Get disk firmware version. """
+        return self._firmware
+
+    def get_disk_number(self) -> int:
+        """Get disk number, as reported by the os."""
+        return self._disk_number
+
+    def get_interface_type(self) -> str:
+        """Get interface type. 
+        
+        Value may be missleading, as some systems may reuse legacy interface 
+        names for newer interface types. 
+        """
+        return self._interface_type
+
+    def get_status(self) -> str:
+        """Get the status of the disk. """
+        return self._status
+
+    def is_media_loaded(self) -> bool:
+        """Is some media inserted in drive. 
+        
+        May make a difference on card readers and such.
+        """
+    
     def is_removable(self) -> bool:
-        """Get wether disk is removable"""
-        if self.media_type == "Removable Media":
+        """Get wether disk is removable."""
+        if self._media_type == "Removable Media":
             return True
         return False
 
-    def get_type(self) -> str:
-        """Get the disk type"""
-        return self.media_type
-
-    def get_partitions(self) -> list:
-        """Get a list of partition objects"""
-        return self.partitions[:]
-
-    def get_caption(self) -> str:
-        """Get model name and maybe manufacturer"""
-        return self.caption
-    
-    def set_path(self, path: str) -> None:
-        """Set drive access path and drive number."""
-        self.path = path
-
     def __str__(self) -> str:
         """Overloading the string method"""
-        disk = ", ".join((self.get_caption(), self.get_path(), self.get_type(), self.get_size()))
-        partitions = ["\n".join(["    " + line for line in str(partition).split("\n")]) for partition in self.partitions]
-        try:   
-            return "\n".join( (disk, *partitions ) )
-        except IndexError:
-            return disk
+        disk = ", ".join(("Disk {}".format(self.get_disk_number()), self.get_path(), self.get_media_type(), human_readable_units(self.get_size())))
+        partitions = ["\n".join(["  " + line for line in str(partition).split("\n")]) for partition in self._partitions]
+        return "\n".join( (disk, *partitions ) )
+
+class LinuxPhysicalDisk(PhysicalDisk):
+    pass
 
 
-class WindowsPhysicalDrive(PhysicalDrive):
+class WindowsPhysicalDisk(PhysicalDisk):
     """Subclass of PhysicalDrive that handles special windows situations"""
-    def __init__(self, wmi_physical_disk: wmi._wmi_object) -> None:
-        super().__init__()
+    def __init__(self, wmi_physical_disk: wmi._wmi_object, system: System) -> None:
+        super().__init__(system)
         self._set_partitions(wmi_physical_disk)
         self._set_size(wmi_physical_disk)
-        self._set_drive_number(wmi_physical_disk)
-        self.path = wmi_physical_disk.DeviceID
-        self._set_media_type = 
+        self._set_disk_number(wmi_physical_disk)
+        self._path = wmi_physical_disk.DeviceID
+        self._set_media_type = wmi_physical_disk.MediaType
+        self._serial_number = wmi_physical_disk.SerialNumber
+        self._model = wmi_physical_disk.Model
+        self._set_sectors(wmi_physical_disk)
+        self._set_heads(wmi_physical_disk)
+        self._set_cylinders(wmi_physical_disk)
+        self._set_bytes_per_sector(wmi_physical_disk)
+        self._set_firmware(wmi_physical_disk)
+        self._interface_type = wmi_physical_disk.InterfaceType
+        self._media_loaded = wmi_physical_disk.MediaLoaded
+        self._status = wmi_physical_disk.Status
         
     def _set_partitions(self, wmi_physical_disk: wmi._wmi_object) -> None:
         """Private method that parses the partitons"""
-        self.partitions = [ WindowsPartition(partition, self) for partition in wmi_physical_disk.associators('Win32_DiskDriveToDiskPartition') ]
+        self._partitions = [ WindowsPartition(partition, self) for partition in wmi_physical_disk.associators('Win32_DiskDriveToDiskPartition') ]
 
     def _set_size(self, wmi_physical_disk: wmi._wmi_object) -> None:
         """set size of disk in bytes."""
         try:
-            self.size = int(wmi_physical_disk.Size)
+            self._size = int(wmi_physical_disk.Size)
         except ValueError:
-            self.size = -1
+            self._size = -1
 
-    def _set_drive_number(self, wmi_physical_disk: wmi._wmi_object) -> None:
-        """set system drive identification number"""
+    def _set_disk_number(self, wmi_physical_disk: wmi._wmi_object) -> None:
+        """set system disk identification number"""
         try:
-            self.disk_number = int(wmi_physical_disk.Index)
-        except IndexError:
-            self.disk_number = -1
+            self._disk_number = int(wmi_physical_disk.Index)
         except ValueError:
-            self.path = ""
-            self.disk_number = -1
+            self._disk_number = -1
+
+    def _set_sectors(self, wmi_physical_disk: wmi._wmi_object) -> None:
+        """Set total number of sectors, or -1 if not a number."""
+        try:
+            self._sectors = int(wmi_physical_disk.TotalSectors)
+        except ValueError:
+            self._sectors = -1
+
+    def _set_heads(self, wmi_physical_disk: wmi._wmi_object) -> None:
+        """Set total number of heads, or -1 if not a number"""
+        try:
+            self._heads = int(wmi_physical_disk.TotalHeads)
+        except ValueError:
+            self._heads = -1
+
+    def _set_cylinders(self, wmi_physical_disk: wmi._wmi_object) -> None:
+        """Set total number of cylinders, or -1 if not a number"""
+        try:
+            self._cylinders = int(wmi_physical_disk.TotalCylinders)
+        except ValueError:
+            self._cylinders = -1
+
+    def _set_bytes_per_sector(self, wmi_physical_disk: wmi._wmi_object) -> None:
+        """Set bytes per sector, or -1 if not a number"""
+        try:
+            self._bytes_per_sector = int(wmi_physical_disk.BytesPerSector)
+        except ValueError:
+            self._bytes_per_sector = -1
+
+    def _set_firmware(self, wmi_physical_disk: wmi._wmi_object) -> None:
+        """Set firmware"""
+        try:
+            self._firmware = wmi_physical_disk.FirmWare
+        except AttributeError:
+            self._firmware = "Unspecified"
 
 
-
-class Partition():
+class Partition:
     """Contains information about partitions. This includes logical disk, 
     or 'volume' information. The same volume may span several disks, and the 
     filesystem of the partition, will in those cases be the filesystem of the 
@@ -213,14 +377,8 @@ class Partition():
     filesystem on its own, though its contents will be part of one.
     """
 
-    def __init__(self, partition) -> None:
-        self._partition = partition
-        self.disk = None
-        self.volume = None
-        self._set_volumes()
-        self._set_size()
-        self._set_partition_table_type()
-        self._set_descriptor()
+    def __init__(self, disk: PhysicalDisk) -> None:
+        self.disk = disk
 
     def get_size(self) -> int:
         """Get the partition size in bytes."""
@@ -234,88 +392,54 @@ class Partition():
         """get the partition table type"""
         return self.partition_table_type
 
-    def _set_descriptor(self) -> None:
+    def __str__(self) -> str:
+        """overloading the __str__ method"""
+        partition = ", ".join((self.get_descriptor(), self.get_partiton_table_type(), human_readable_units(self.get_size())))
+        return "\n".join((partition, ))
+
+
+class LinuxPartition(Partition):
+    pass
+
+class WindowsPartition(Partition):
+    def __init__(self, partition: wmi._wmi_object, disk: PhysicalDisk) -> None:
+        super().__init__(disk)
+        self._set_descriptor(partition)
+        self._set_size(partition)
+        self._set_partition_table_type(partition)
+
+    def _set_descriptor(self, partition: wmi._wmi_object) -> None:
         """private: extract partition descriptor from a wmi object"""
-        self.descriptor = self._partition.Name
+        self.descriptor = partition.Name
 
-    def _set_volumes(self) -> None:
-        """Private: parse the logical disks"""
-        self.volumes = [ Volume(volume) for volume in self._partition.associators('Win32_LogicalDiskToPartition')]
-
-    def _set_size(self) -> None:
+    def _set_size(self, partition: wmi._wmi_object) -> None:
         """private: extract size from a wmi object"""
         try:
-            self.size = int(self._partition.Size)
+            self.size = int(partition.Size)
         except ValueError:
             self.size = 0
 
-    def _set_partition_table_type(self) -> None:
+    def _set_partition_table_type(self, partition: wmi._wmi_object) -> None:
         """private: extract partition table type from a wmi object"""
-        self.partition_table_type = self._partition.Type
+        self.partition_table_type = partition.Type
 
-    def __str__(self) -> str:
-        """overloading the __str__ method"""
-        partition = ", ".join((self.get_descriptor(), self.get_partiton_table_type(), str(self.get_size())))
-        volumes = []
-        volumes = ["\n".join(["    " + line for line in str(volume).split("\n")]) for volume in self.volumes]
-        try:
-            return "\n".join((partition, *volumes))
-        except IndexError:
-            return partition
+    
 
 
-class Volume(object):
-    def __init__(self, volume) -> None:
-        self._volume = volume
-        self._set_name()
-        self._set_filesystem()
-
-    def get_file_system(self) -> str:
-        """Get filesystem type"""
-        return self.file_system
-
-    def get_name(self) -> str:
-        """Get the volume path letter."""
-        return self.name
-
-    def _set_filesystem(self) -> None:
-        """Private: Extracts the filesystem type from a wmi object"""
-        if isinstance(self._volume.FileSystem, str):
-            self.file_system = self._volume.FileSystem
-        else:
-            self.file_system = ""
-
-    def _set_name(self) -> None:
-        """Private: Extracts the volume name from a wmi object"""
-        if isinstance(self._volume.Name, str):
-            self.name = self._volume.Name
-        else:
-            self.name = ""
+class LogicalDisk:
+    """Class for logical disks/mount points"""    
     
     def __str__(self) -> str:
-        """overloading the __str__ method"""
-        return ", ".join((self.get_name(), self.get_file_system()))
+        return "Logical Disk"
 
 
-def get_disks(computer: str = "", username: str = "", password: str = "") -> list:
-    """Reads the system information and returns a list of disk information.
-    The list consist of Disk objects. The Disk object will include the partiton
-    objects
+class LinuxLogicalDisk(LogicalDisk):
+    pass
 
-    You may supply computer, username and password to connect to other
-    computers. But no concideration is taken for the protection of credentials
-    in memory. Username need to include domain/workgroup. Like domain\\username
-    """
-    try:
-        wmi_cursor = wmi.WMI(computer=computer, user=username, password=password)
-    except wmi.x_access_denied as err:
-        return None
-    except wmi.x_wmi_authentication as err:
-        return None
-    disks = [PhysicalDrive(physical_disk) for physical_disk in wmi_cursor.Win32_DiskDrive()]
-    return disks
+class WindowsLogicalDisk(LogicalDisk):
+    pass
 
 
 if __name__ == "__main__":
-    disk = System()
-    print(str(disk))
+    system = System()
+    print(str(system))
