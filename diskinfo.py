b7532e6f60616063b299e0485d8f16020a4f35f3
Lars Henrik Ericson <glarrrrgh@gmail.com>
2022-04-08 09:47:08 +0200

    moved human_readable_units out of the main module

--- diskinfo.py
+++ diskinfo.py
@@ -35,85 +35,10 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import sys
+from human_readable_units import UNITS, human_readable_units
 
 if sys.platform == 'win32':
     import wmi
-
-UNITS = { '': 1,
-          'K': 1_000, 
-          'M': 1_000_000, 
-          'G': 1_000_000_000, 
-          'T': 1_000_000_000_000, 
-          'P': 1_000_000_000_000_000,
-          'B': 1,
-          'KB': 1_000, 
-          'MB': 1_000_000, 
-          'GB': 1_000_000_000, 
-          'TB': 1_000_000_000_000, 
-          'PB': 1_000_000_000_000_000,
-          'KiB': 1_024, 
-          'MiB': pow(1_024, 2), 
-          'GiB': pow(1_024, 3), 
-          'TiB': pow(1_024, 4), 
-          'PiB': pow(1_024, 5)
-        }
-
-def human_readable_units(value: int, 
-                         unit: str = 'auto', 
-                         value_type: str = 'B', 
-                         decimal_places: int=2
-                         ) -> str:
-    """Converts an int to a string with a unit. 
-    
-       If unit is omitted, the function will choose a fitting unit.
-       If unit is omitted, and type is omitted, it will choose something like 
-       KB. type can be 
-            'B': decimal bytes (KB for instance)
-            'M': no bytes (K for instance)
-            'I': binary bytes (KiB for instance)
-       If unit is specified, type will be ignored.
-       decimal_places describes number of places after the dot. It must be a 
-       number between and including 0 and 9.
-    """
-    if unit == 'auto':
-        if value_type == 'M':
-            for each_unit in ('P', 'T', 'G', 'M', 'K', ''):
-                return_value = value/UNITS[each_unit]
-                return_unit = each_unit
-                if return_value > 1:
-                    break            
-        elif value_type == 'I':
-            for each_unit in ('PiB', 'TiB', 'GiB', 'MiB', 'KiB', 'B'):
-                return_value = value/UNITS[each_unit]
-                return_unit = each_unit
-                if return_value > 1:
-                    break
-        else:
-            for each_unit in ('PB', 'TB', 'GB', 'MB', 'KB', 'B'):
-                return_value = value/UNITS[each_unit]
-                return_unit = each_unit
-                if return_value > 1:
-                    break        
-    else:
-        return_unit = unit
-        try:
-            return_value = value/UNITS[unit]
-        except KeyError:
-            raise ReadableUnitError
-    if type(decimal_places) != int or decimal_places < 0 or decimal_places > 9:
-        decimal_places = 2
-    if return_unit == 'B' or return_unit =='':
-        decimal_places = 0
-    format_string = "{:." + str(decimal_places) + "f}{}"
-    return format_string.format(return_value, return_unit)
-
-
-class ReadableUnitError(Exception):
-    """Exception raised if unit specified is not in dict UNITS"""
-    def __init__(self, message, status):
-        super().__init__(message, status)
-        self.message = "unit specified not found in UNITS"
-        self.status = status
 
 
 class DiskInfoParseError(Exception):
