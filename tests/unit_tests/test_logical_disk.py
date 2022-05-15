ae8695f177a37ffcd01d00e1184eb219f4382a3b
glarrrgh <glarrrgh@gmail.com>
2022-05-15 16:15:13 +0200

    refactor: removed logical_disk.py

--- tests/unit_tests/test_logical_disk.py
+++ tests/unit_tests/test_logical_disk.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from src.pydiskinfo.logical_disk import LogicalDisk
+from src.pydiskinfo import LogicalDisk
 from src.pydiskinfo.windows_system import WindowsLogicalDisk
 
 
